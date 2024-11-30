f6cab58a0ec8fd54f495c25f801a3277277069b8
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-11-30 11:11:45 -0700

    added mouseenter event listener and handleMouseEnter to change the background of the square to black when it is triggered

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector("#container")
+const row = document.createElement("div")
+const square = document.createElement("div")
 
 for (let i = 0; i < 16; ++i) { // creates the square grid
     const row = document.createElement("div")
@@ -6,9 +8,12 @@ for (let i = 0; i < 16; ++i) { // creates the square grid
     for (let j = 0; j < 16; ++j) {
         const square = document.createElement("div")
         square.classList.add("square")
+        square.addEventListener("mouseenter", handleMouseEnter)
         row.appendChild(square)
     }
     container.appendChild(row)
 }
 
-square.addEventListener(mouseenter, handleMouseEnter)
+function handleMouseEnter(event) {
+    event.target.setAttribute("style", "background: black")
+}
