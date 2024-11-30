04f5868ba27738567d7f38b77fde39b1c210d5aa
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-11-29 23:45:44 -0700

    setting up hover effect with mouseenter event listener

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const container = document.querySelector("#container")
 
-for (let i = 0; i < 16; ++i) {
+for (let i = 0; i < 16; ++i) { // creates the square grid
     const row = document.createElement("div")
     row.classList.add("row")
     for (let j = 0; j < 16; ++j) {
-        const square = document.createElement("p")
+        const square = document.createElement("div")
         square.classList.add("square")
         row.appendChild(square)
     }
     container.appendChild(row)
 }
+
+square.addEventListener(mouseenter, handleMouseEnter)
