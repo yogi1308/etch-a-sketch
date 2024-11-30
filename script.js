12f45970334444c22c23cb0cd148c549abb2bef9
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-11-30 11:57:02 -0700

    made a new function createGrid that uses the for loop in preveious versions and takes numberOfSqaures as argument value and has 16 as the default value.
    made a new function removeGrid that erases the
    pre-existing grid

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const container = document.querySelector("#container")
 const row = document.createElement("div")
 const square = document.createElement("div")
-
-for (let i = 0; i < 16; ++i) { // creates the square grid
-    const row = document.createElement("div")
-    row.classList.add("row")
-    for (let j = 0; j < 16; ++j) {
-        const square = document.createElement("div")
-        square.classList.add("square")
-        square.addEventListener("mouseenter", handleMouseEnter)
-        row.appendChild(square)
-    }
-    container.appendChild(row)
-}
 const button = document.querySelector("button")
 button.addEventListener("click", handlebuttonClick)
+createGrid()
+
+function createGrid(numberOfSquares = 16) {
+    for (let i = 0; i < numberOfSquares; ++i) { // creates the square grid
+        const row = document.createElement("div")
+        row.classList.add("row")
+        for (let j = 0; j < numberOfSquares; ++j) {
+            const square = document.createElement("div")
+            square.classList.add("square")
+            square.addEventListener("mouseenter", handleMouseEnter)
+            row.appendChild(square)
+        }
+        container.appendChild(row)
+    }
+}
 
 
 function handleMouseEnter(event) {
@@ -23,4 +26,13 @@ function handleMouseEnter(event) {
 
 function handlebuttonClick(event) {
     numberOfSquares = prompt("Number of squares per side?")
+    removeGrid()
+    createGrid(numberOfSquares)
+}
+
+function removeGrid() {
+    const container = document.querySelector("#container")
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
 }
