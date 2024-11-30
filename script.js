08a513239b76c798c354dce5394442b0e0a90d3c
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-11-29 23:28:17 -0700

    used js to make 16 div squares and used css to make them fit the container

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector("#container")
+
+for (let i = 0; i < 16; ++i) {
+    const row = document.createElement("div")
+    row.classList.add("row")
+    for (let j = 0; j < 16; ++j) {
+        const square = document.createElement("p")
+        square.classList.add("square")
+        row.appendChild(square)
+    }
+    container.appendChild(row)
+}
